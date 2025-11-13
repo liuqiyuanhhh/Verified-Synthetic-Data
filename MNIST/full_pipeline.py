284f003d8eb85b25dc9468cb4c8379218c6c798b
yuwei cheng <yuwei.cheng@u.nus.edu>
2025-11-12 18:35:57 -0600

    Rose tests work flow

--- MNIST/full_pipeline.py
+++ MNIST/full_pipeline.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import os
 import random
 
+# test of commit
 vae_path = "/home/qiyuanliu/data_filter/Verified-Synthetic-Data/MNIST/conv_cvae"
 sys.path.append(vae_path)
 
